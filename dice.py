8fd4cb27e652131a325ca23cc7ce563d58d7a4c4
Leonardo Vilbert <69805454+Levi-Hawks@users.noreply.github.com>
2020-12-18 23:01:00 -0300

    Add files via upload
    Script completo.

---
+++ dice.py
@@ -0,0 +1,44 @@
+from random import randint
+
+default_dice = randint(1, 6)
+
+while True:
+
+    roll = input('Deseja rolar o dado? ')
+
+    elif roll.isalpha() != True:
+        print("Digite apenas 'sim' ou 'não'.")
+        continue
+ 
+    elif roll[0].lower() == 'n':
+        print('Ok, o programa será fechado.')
+        break
+    
+    elif roll[0].lower() == 's':
+
+        while True:
+            default_roll = input('Deseja rolar o dado default (6)? ')
+
+            elif default_roll.isalpha() != True:
+                print("Digite apenas 'sim' ou 'não'.")
+                continue
+
+            elif default_roll[0].lower() == 's':
+                print(f'O resultado foi {default_dice}.')
+                break
+            
+            elif default_roll[0].lower() == 'n':
+
+                while True:
+
+                    new_dice = input('Você deseja rolar um dado de quantos lados? ')
+
+                    if new_dice.isnumeric() == True:
+                        print(f'O resultado foi {randint(1, int(new_dice))}.')
+                        break        
+
+                    else:
+                        print('Digite um número.')
+                        continue
+
+            break
